be698726bf0ce3d44a0dd658017a4d15f3cbcc6a
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-08-16 13:51:45 -0600

    Fixed up Venue page

---
+++ client/SongRequest.jsx
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+var React = require('React');
+
+var Modal = require('react-bootstrap').Modal;
+var Button = require('react-bootstrap').Button;
+
+
+module.exports = SongRequest;
--- client/Venue.jsx
+++ client/Venue.jsx
@@ -6,10 +6,29 @@ var Grid = require('react-bootstrap').Grid;
 var Row = require('react-bootstrap').Row;
 var Col = require('react-bootstrap').Col;
 var Table = require('react-bootstrap').Table;
+var Modal = require('react-bootstrap').Modal;
+var ModalTrigger = require('react-bootstrap').ModalTrigger;
+var Button = require('react-bootstrap').Button;
 
 var Map = require('./Map');
 var styles = require('./styles');
 
+
+var SongRequest = React.createClass({
+    render: function() {
+        return this.transferPropsTo(
+            <Modal title="Request a song" animation={true}>
+                <div className="modal-body">
+                    <h4>Request a Song!</h4>
+                    <p>Here is where you can request a song!</p>
+                </div>
+                <div className="modal-footer">
+                    <Button onClick={this.props.onRequestHide}>Submit</Button>
+                </div>
+            </Modal>
+        );
+    }
+});
 
 var Venue = React.createClass({
     render: function() {
@@ -18,13 +37,19 @@ var Venue = React.createClass({
         var padding = {
             padding: '0px 0px 30px 0px'
         };
+        var center = {
+            'text-align': 'center'
+        };
+        var right = {
+            'text-align': 'right'
+        };
         return (
             <Grid style={styles.OXYGEN}>
-                <Row style={padding} >
+                <Row>
                     <Col md={3} >
-                        <Row styles={styles.CENTER} >
-                            <Col md={6} >
-                                <a href={url} style={styles.CENTER}>
+                        <Row>
+                            <Col md={6} style={center}>
+                                <a href={url}>
                                     <img src="/static/images/viz.png" />
                                 </a>
                             </Col>
@@ -44,42 +69,25 @@ var Venue = React.createClass({
                         </Row>
                     </Col>
                     <Col md={6} >
-                        <Table striped bordered condensed hover>
-                            <thead>
-                                <tr>
-                                    <th>Activity</th>
-                                    <th>Time</th>
-                                    <th>Details</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Ceremeony</td>
-                                    <td>5:30 pm</td>
-                                    <td>Will be outdoors if possible</td>
-                                </tr>
-                                <tr>
-                                    <td>Cocktails</td>
-                                    <td>6:00 pm</td>
-                                    <td>Yummy drinks!</td>
-                                </tr>
-                                <tr>
-                                    <td>Dinner</td>
-                                    <td>6:30 pm</td>
-                                    <td>Bon Apetit!</td>
-                                </tr>
-                                <tr>
-                                    <td>Cake!</td>
-                                    <td>7:30 pm</td>
-                                    <td>A special treat to share with you</td>
-                                </tr>
-                                <tr>
-                                    <td>Non-optional dancing!</td>
-                                    <td>8:30 pm - ?</td>
-                                    <td>Bring your dancing shoes!</td>
-                                </tr>
-                            </tbody>
-                        </Table>
+                        <Row style={padding}>
+                            <Col md={4} style={right} >
+                                <b>Ceremony</b> 5:30 pm
+                            </Col>
+                            <Col md={8}>
+                                Will be outdoors weather permitting so bring something to keep you warm in case it is chilly
+                            </Col>
+                        </Row>
+                        <Row style={padding}>
+                            <Col md={12} style={center}>
+                                <b>Cocktails and hors d'oeuvres</b> to follow
+                            </Col>
+                        </Row>
+                        <Row style={padding}>
+                            <Col md={12} style={center}>
+                                <b>Dinner and Dancing</b> 7:00pm - 11:00 in the pavilion
+                            </Col>
+                        </Row>
+
                     </Col>
                     <Col md={3} >
                         <Map latitude={38.5645001} longitude={-121.4835314} zoom={16} width={250} height={250} points={[{latitude:38.5645001,longitude:-121.4835314,title:"Vizcaya"}]} />
@@ -89,5 +97,18 @@ var Venue = React.createClass({
         );
     }
 });
+
+
+/*
+<Row style={center} >
+    <Col md={12} >
+        <span>Know a good song to dance to?</span>
+        <ModalTrigger modal={<SongRequest />} style={padding: '0px 10px 0px 10px'} >
+            <Button>Click here</Button>
+        </ModalTrigger>
+        <span>to request one!</span>
+    </Col>
+</Row>
+*/
 
 module.exports = Venue;
