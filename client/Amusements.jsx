7e5933b372bb8998d2bf390102512c3b4daa52c4
Bovard Tiberi <bovard.tiberi@gmail.com>
2015-01-07 21:54:46 -0700

    updated

--- client/Amusements.jsx
+++ client/Amusements.jsx
@@ -11,38 +11,29 @@ var Interest = React.createClass({
     propTypes: {
         imgUrl: React.PropTypes.string.isRequired,
         url: React.PropTypes.string.isRequired,
-        distance: React.PropTypes.number.isRequired,
         desc: React.PropTypes.string.isRequired,
-        title: React.PropTypes.string.isRequired
+        title: React.PropTypes.string.isRequired,
+        map: React.PropTypes.string.isRequired,
+        price: React.PropTypes.string
     },
     render: function() {
+        var price;
+        if (this.props.price) {
+            price = " | " + this.props.price;
+        }
+        console.log(price);
         return (
             <Col xs={12} md={6}>
-                <Grid>
-                    <Row style={{padding: '0px 0px 10px 0px'}}>
-                        <Col xs={12} md={6}>
-                            <h3 style={styles.OXYGEN}>{this.props.title}</h3>
-                        </Col>
-                    </Row>
-                    <Row style={{padding: '0px 0px 10px 0px'}}>
-                        <Col xs={8} md={4}>
-                            <img style={{'max-width': '100%'}} src={this.props.imgUrl} />
-                        </Col>
-                        <Col xs={4} md={2}>
-                            <Row>
-                                <a href={this.props.url}><p style={styles.OXYGEN}>{this.props.url}</p></a>
-                            </Row>
-                            <Row>
-                                <p style={styles.OXYGEN}>Distance: {this.props.distance} miles</p>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12} md={6}>
-                            <p style={styles.OXYGEN}>{this.props.desc}</p>
-                        </Col>
-                    </Row>
-                </Grid>
+                <a href={this.props.url}>
+                    <h3 style={styles.OXYGEN}>{this.props.title}</h3>
+                    <img style={{'max-width': '100%', 'height': '100px'}} src={this.props.imgUrl} />
+                </a>
+                <div style={{'height': '100px', 'padding': '10px 0 0 0'}}>
+                    <p style={styles.OXYGEN}>
+                        <a href={this.props.map}>Map</a> {price}
+                    </p>
+                    <p style={styles.OXYGEN}>{this.props.desc}</p>
+                </div>
             </Col>
         );
     }
@@ -51,18 +42,52 @@ var Interest = React.createClass({
 
 var interests = [
     {
+        title: 'Trick Pony',
+        imgUrl: 'static/images/amusements/trickpony.00_png_srz',
+        url: 'http://www.trickponypizza.com/',
+        desc: "Colette and her mom enjoyed a delicious wood-fired pizza at Trick Pony over the holidays!",
+        map: 'https://www.google.com/maps/place/2534+Industrial+Blvd+%23110,+West+Sacramento,+CA+95691/@38.5660149,-121.5434754,17z/data=!3m1!4b1!4m2!3m1!1s0x809ad3f9467567e1:0x615ec81bd94247fc',
+        price: '$$'
+    },
+    {
+        title: 'Biba Restaurant',
+        imgUrl: 'static/images/amusements/biba.jpg',
+        url: 'http://www.biba-restaurant.com/',
+        desc: "Authentic Italian food that has great reviews. Colette's parents have not been to this restaurant yet, but are excited to check it out!",
+        map: 'https://www.google.com/maps/place/2801+Capitol+Ave,+Sacramento,+CA+95816/@38.570755,-121.470114,17z/data=!3m1!4b1!4m2!3m1!1s0x809ad0eae1cfbb2f:0x208060f73b367ff',
+        price: '$$$'
+    },
+    {
+        title: 'Bike Dog Brewing Company',
+        imgUrl: 'static/images/amusements/bikedog.png',
+        url: 'http://bikedogbrewing.com/',
+        desc: "Colette's parents said the beer here was great and the brewery had a fun atmosphere! There are rotating food trucks that park next to the brewery if you want some food with your beer.",
+        map: '',
+        price: '$$'
+    },
+    {
+        title: 'Jackrabbit Brewing',
+        imgUrl: 'static/images/amusements/jackrabbit.jpg',
+        url: 'http://www.jackrabbitbrewingcompany.com/',
+        desc: "Heard positive reviews about this new brewery in West Sacramento.",
+        map: 'https://www.google.com/maps/place/1315+Terminal+St,+West+Sacramento,+CA+95691/@38.5721169,-121.5413686,17z/data=!3m1!4b1!4m2!3m1!1s0x809ad400bf92a5c1:0x69bd3387e2ec010b',
+        price: '$$'
+    },
+    {
         title: 'Amador County Wine Country',
         imgUrl: 'static/images/amusements/amador.jpg',
         url: 'http://www.amadorwine.com/',
-        distance: 40,
-        desc: "Amador County wine country is a refreshing discovery for those with a fascination for wine. Only 45 minutes from the State Capital of Sacramento and two hours from the San Francisco Bay Area, you'll be welcomed with warmth and good taste."
+        desc: "Amador County wine country is a refreshing discovery for those with a fascination for wine.",
+        map: 'http://www.amadorwine.com/visit-us/map-directions/',
+        price: '$$'
     },
     {
         title: 'Placer Country Wine Country',
         imgUrl: 'static/images/amusements/placer.jpg',
         url: 'http://www.placerwine.com/',
-        distance: 35,
-        desc: "The Placer County Wine Trail is a rustic wine escape within a stone's throw to city life. With a diverse collection of boutique wineries in the Auburn, Lincoln, Loomis, Newcastle, Rocklin circle and Meadow Vista, our winemakers are here for a warm greeting and are eager to share the knowledge and passion of their craft and award-winning appellation with all levels of enthusiasts. Fall in love with the beauty and bounty with a day or weekend trip to The Placer County Wine Trail."
+        desc: "The Placer County Wine Trail is a rustic wine escape within a stone's throw to city life.",
+        map: 'http://www.placerwine.com/assets/placer_wine_trail.pdf',
+        price: '$$'
     }
 ];
 
@@ -74,8 +99,10 @@ var Amusements = React.createClass({
                 <Interest
                     title={obj.title}
                     imgUrl={obj.imgUrl}
+                    id={obj.imgUrl}
                     url={obj.url}
-                    distance={obj.distance}
+                    map={obj.map}
+                    price={obj.price}
                     desc={obj.desc} />
             );
         });
